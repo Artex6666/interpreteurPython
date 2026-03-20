5c5154d468569f32f7635c64da28aa5206bf8ced
Baptiste Coquet <bcoquet03@gmail.com>
2026-03-20 17:38:32 +0100

    refact: complete

--- src/ast/node/__init__.py
+++ src/ast/node/__init__.py
@@ -1,6 +1,6 @@
 all = ["args","binary","params","call"]
 
-from . import args
-from . import binary
-from . import params
-from . import call
+from . import args_node
+from . import binary_node
+from . import params_node
+from . import call_node
--- src/ast/node/params_node.py
+++ src/ast/node/params_node.py
@@ -4,6 +4,7 @@ from ast.ast_node import AstNode
 class ParamsNode(AstNode):
     def __init__(self,args):
         super().__init__("ParamsNode",args or [])
+        self.len = args.__len__()
 
     def __repr__(self):
         class_name = type(self).__name__
